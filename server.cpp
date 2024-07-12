ba6c5c2104f4f94720e35a99ed4fff7753778223
Sumeyye Aydogan <sumeyyednm1@gmail.com>
2024-07-12 16:05:09 +0300

    start for server

---
+++ server.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h> //for inetpton
+#include <tchar.h>
+
+using namespace std;
+
+#pragma comment(lib, "ws2_32.lib")
+
+
+/*
+    //initialize winsock library
+
+    //create the socket
+
+    //get ip and port
+
+    //bind the ip/port with the socket
+
+    //listen on the socket
+
+    //accept
+
+    //recv and send
+
+    //close the socket
+
+    //clean up the winsock
+
+*/
+
+bool Initialize(){
+    WSADATA data;
+    return WSAStartup(MAKEWORD(2,2), &data) == 0;
+
+}
+
+int main(){
+    if(!Initialize()){
+        cout << "winsock initialization failed" << endl;
+
+    }
+
+    cout << "server program" << endl;
+
+    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(listenSocket == INVALID_SOCKET){
+        cout << "socket creation failed" << endl;
+        return 1;
+    }
+
+    //create address structure
+    sockaddr_in serveraddr;
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(12345);
+
+    //convert the ipaddress(0.0.0.0) put it inside the sin_family in binary form
+    if (InetPton(AF_INET, _T("0.0.0.0"), &serveraddr.sin_addr) != 1){
+        cout << "setting address structure failed " << endl;
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    //bind 
+    if (bind(listenSocket, reinterpret_cast<sockaddr*>(&serveraddr), sizeof(serveraddr)) == SOCKET_ERROR){
+        cout << "bind failed" <<endl;
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    WSACleanup();
+    return 0;
+}
